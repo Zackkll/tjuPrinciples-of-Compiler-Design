4dd2c7769a1bef23095c076fbbf841f404cfc623
吕睿哲 <1034167849@qq.com>
2024-11-16 21:20:56 +0800

    debug

--- src/code/Mainpaser.java
+++ src/code/Mainpaser.java
@@ -2,7 +2,7 @@ package code;
 import java.util.*;
 public class Mainpaser {
 	public static void main(String[] args) {
-        String code = "int x = 5; float y = 1.1;";
+        String code = "int x = 5; float y = 1;";
         Lexer lexer = new Lexer();
         List<Token> tokens = lexer.analyze(code);
 
